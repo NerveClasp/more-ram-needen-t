425424d113497f0af6b0d2fd01ef1112dfab0692
Roman Horokhov <7347253+NerveClasp@users.noreply.github.com>
2024-11-19 04:06:15 +0200

    feat: add line count

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use regex::Regex;
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
 use std::path::PathBuf;
 
 #[derive(Parser)]
@@ -26,7 +26,13 @@ fn main() {
         // HashMap to count IP occurrences
         let mut ip_counts: HashMap<String, usize> = HashMap::new();
 
+        let mut line_count: u64 = 0;
+
         for line in reader.lines() {
+            line_count += 1;
+            print!("\rLines processed: {}", line_count);
+            std::io::stdout().flush().unwrap();
+
             let line = line.unwrap();
             // change this regex if needed, make sure to have a capture group `(blah)`
             let re = Regex::new(r"Source IP: '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'");
@@ -46,7 +52,7 @@ fn main() {
             }
         }
         // Display the results as a table
-        println!("{:<20} Count", "IP Address");
+        println!("\n{:<20} Count", "IP Address");
         println!("{:-<20} -----", "-");
         for (ip, count) in ip_counts {
             println!("{:<20} {}", ip, count);
