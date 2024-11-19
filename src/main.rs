007f7006d9f64b1657cb47ef1d18ce17cbd6c46b
Roman Horokhov <7347253+NerveClasp@users.noreply.github.com>
2024-11-19 03:38:11 +0200

    feat: implement extracting and counting IPs using hardcoded regex

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use regex::Regex;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
@@ -22,9 +24,32 @@ fn main() {
         let file = File::open(args.file).unwrap();
         let reader = BufReader::new(file);
 
+        // HashMap to count IP occurrences
+        let mut ip_counts: HashMap<String, usize> = HashMap::new();
+
         for line in reader.lines() {
             let line = line.unwrap();
-            println!("{}", line); // Do something with the line
+            let re = Regex::new(r"Source IP: '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'");
+
+            match re {
+                Ok(re) => {
+                    if let Some(captures) = re.captures(&line) {
+                        if let Some(ip) = captures.get(1) {
+                            let ip_str = ip.as_str().to_string();
+                            *ip_counts.entry(ip_str).or_insert(0) += 1;
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
+                }
+            }
+        }
+        // Display the results as a table
+        println!("{:<20} {}", "IP Address", "Count");
+        println!("{:-<20} {}", "-", "-----");
+        for (ip, count) in ip_counts {
+            println!("{:<20} {}", ip, count);
         }
     } else {
         println!("The file does not exist.");
